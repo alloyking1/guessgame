c0e8fddd119009096fe5c0a3d3eeeae33eaf5d82
alloyking1 <alloyking1@gmail.com>
2021-06-17 08:09:33 -0400

    fetched data from console using bufio and converted to in using strconv packages collectively

--- game.go
+++ game.go
@@ -1,6 +1,28 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+	)
 
 func main(){
-	fmt.Println("hello world")
+	fmt.Printf("Guess a number between 1-5: \n")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	ran := numGenerate()
+
+	if ran == input {
+		fmt.Printf("Your correct %q is the right number", input)
+	}else {
+		fmt.Printf("Your wrong %q is not the generated number", input)
+	}
+}
+
+func numGenerate() int64 {
+	var num int64
+	num = 22
+	return num
 }
